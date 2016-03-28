2ff1c0c0be96c1989a46c7be1f4a15796f333ec5
meek <otmeek@gmail.com>
2016-03-28 03:51:19 +0100

    set up

---
+++ server.js
@@ -0,0 +1,23 @@
+// setup ==============================================
+var express    = require('express');
+var mongoose   = require('mongoose');
+var morgan     = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+require('dotenv').load();
+
+// config =============================================
+mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// listen =============================================
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
